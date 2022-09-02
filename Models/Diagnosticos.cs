725c340e99077be7e17b7f914070461660fe91a5
Felipe Diniz <fdsampaio@yahoo.com.br>
2022-09-01 22:28:34 -0300

    Ajuste na exibição de Diagnosticos no Swagger

--- Models/Diagnosticos.cs
+++ Models/Diagnosticos.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace DesafioAPIConsultasVeterinarias.Models
 {
     public class Diagnosticos
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Diagnóstico é obrigatório")]
